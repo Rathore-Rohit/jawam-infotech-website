e708d243c0342da54950e15df1d578a1aba61b03
Rohit Rathore <rohitrathore1130@gmail.com>
2025-04-28 11:44:39 +0530

    Completed About Us page and refactored Why Choose Us section into a new component

--- src/components/ui/ServicesMainSection.jsx
+++ src/components/ui/ServicesMainSection.jsx
@@ -3,53 +3,20 @@ import { Box, Grid, Typography } from "@mui/material";
 import theme from "../../theme";
 import Underline from "../../components/ui/Underline";
 import CheckIcon from "@mui/icons-material/Check";
-import whyChooseUs from "../../assets/whyChooseUs.png";
-import MilitaryTechIcon from "@mui/icons-material/MilitaryTech";
-import PsychologyIcon from "@mui/icons-material/Psychology";
-import GroupsIcon from "@mui/icons-material/Groups";
-import SupportAgentIcon from "@mui/icons-material/SupportAgent";
+import WhyChooseUs from "./WhyChooseUs";
 
-const commonTypographyStyles = {
-  color: theme.palette.custom.paragraph,
-  fontSize: "16px",
-  fontWeight: 400,
-  fontFamily: theme.typography.fontFamilySecondary,
-  lineHeight: "24px",
-  textAlign: "justify",
-  mb: 3,
-};
-
-const titleTypographyStyles = {
-  color: theme.palette.text.secondary,
-  fontSize: { xs:"21px", sm: "22px", md: "24px" },
-  fontWeight: 700,
-  fontFamily: theme.typography.fontFamily,
-};
-
-const descTypographyStyles = {
-  color: theme.palette.custom.paragraph,
-  fontSize: "16px",
-  fontWeight: 400,
-  fontFamily: theme.typography.fontFamilySecondary,
-  lineHeight: "24px",
-};
-
-const boxStyles = {
-  height: "50%",
-  display: "flex",
-  flexDirection: "column",
-  gap: 0.5,
-  mb: { xs: 1.5, md: 0 },
-}
-
-const ServicesMainSection = ({ title, heading, description, points, imgSrc }) => {
+const ServicesMainSection = ({
+  title,
+  heading,
+  description,
+  points,
+  imgSrc,
+}) => {
   return (
     <>
-      <Box
-        sx={{ px: { xs: 3, sm: 6, lg: 14 }, py: 12 }}
-      >
-        <Grid container spacing={6} alignItems="center"> 
-          {/* Left Text Section */}          
+      <Box component="section" sx={{ px: { xs: 3, sm: 6, lg: 14 }, py: 12 }}>
+        <Grid container spacing={6} alignItems="center">
+          {/* Left Text Section */}
           <Grid size={{ xs: 12, md: 7 }}>
             <Typography
               component="h5"
@@ -142,127 +109,7 @@ const ServicesMainSection = ({ title, heading, description, points, imgSrc }) =>
       </Box>
 
       {/* Why Choose Us Section */}
-      <Box
-        sx={{
-          px: { xs: 3, sm: 6, lg: 14 },
-          pt: { xs: 2, md: 6, lg: 8 },
-          pb: { xs: 16, md: 18, lg: 16 },
-        }}
-      >
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            px: { lg: 18 },
-          }}
-        >
-          <Typography
-            component="h5"
-            sx={{
-              textTransform: "uppercase",
-              color: theme.palette.text.primary,
-              fontSize: "20px",
-              fontWeight: 800,
-              fontFamily: theme.typography.fontFamily,
-              mb: 2,
-            }}
-          >
-            Why Choose Us ?
-          </Typography>
-          <Typography sx={commonTypographyStyles}>
-            <span style={{ fontWeight: 500 }}>Expertise</span>: At Jawam
-            Infotech, our seasoned professionals bring a wealth of expertise to
-            every project, ensuring a deep understanding of your unique
-            challenges and delivering solutions that go beyond expectations.
-          </Typography>
-          <Typography sx={commonTypographyStyles}>
-            <span style={{ fontWeight: 500 }}>Custom Solutions</span>: We
-            specialize in crafting bespoke solutions tailored to your specific
-            needs. Our commitment to customization ensures that your project is
-            not just an application but a strategic asset designed to elevate
-            your business.
-          </Typography>
-          <Typography sx={commonTypographyStyles}>
-            <span style={{ fontWeight: 500 }}>Quality Assurance</span>: Rigorous
-            testing processes and a dedicated quality assurance team guarantee
-            that your solution is not just functional, but flawless, meeting the
-            highest standards of performance and reliability.
-          </Typography>
-          <Typography sx={commonTypographyStyles}>
-            <span style={{ fontWeight: 500 }}>Timely Delivery</span>: Time is of
-            the essence, and we value yours. With our commitment to timely
-            delivery, we ensure that your project is not only completed on
-            schedule but also exceeds your expectations, providing you a
-            competitive edge in the market.
-          </Typography>
-          <Underline
-            lineWidth="140px"
-            lineHeight="4px"
-            barWidth="5px"
-            mtop={1.2}
-            mBottom={{ xs: 5, sm: 7 }}
-          />
-        </Box>
-        <Box>
-          <Grid container spacing={6}>
-            <Grid size={{ xs: 12, md: 4 }}>
-              <Box sx={boxStyles}>
-                <MilitaryTechIcon sx={{ color: theme.palette.text.primary, fontSize: 50 }} />
-                <Typography component="h4" sx={titleTypographyStyles}>
-                  Best In Industry
-                </Typography>
-                <Typography sx={descTypographyStyles}>
-                  We are setting the standard for being Best-in-Industry
-                  Solutions.
-                </Typography>
-              </Box>
-              <Box sx={boxStyles}>
-                <PsychologyIcon sx={{ color: theme.palette.text.primary, fontSize: 50 }} />
-                <Typography component="h4" sx={titleTypographyStyles}>
-                  Innovation Hub
-                </Typography>
-                <Typography sx={descTypographyStyles}>
-                  Crafting Customer-Centric Solutions in an innovative manner.
-                </Typography>
-              </Box>
-            </Grid>
-            <Grid size={{ xs: 12, md: 4 }}>
-              <Box
-                component="img"
-                src={whyChooseUs}
-                alt="Image"
-                sx={{
-                  width: { xs: "100%", sm: "670px", md: "100%" },
-                  height: { xs: "auto", sm: "350px", md: "auto" },
-                  borderRadius: 2,
-                  mt: { sm: 1.5, md: 6.5, lg: 0 },
-                }}
-              />
-            </Grid>
-            <Grid size={{ xs: 12, md: 4 }}>
-              <Box sx={boxStyles}>
-                <GroupsIcon sx={{ color: theme.palette.text.primary, fontSize: 50 }} />
-                <Typography component="h4" sx={titleTypographyStyles}>
-                  Professional Staff
-                </Typography>
-                <Typography sx={descTypographyStyles}>
-                  Empowering Excellence with a Team of Dedicated and
-                  Professional Staff.
-                </Typography>
-              </Box>
-              <Box sx={boxStyles}>
-                <SupportAgentIcon sx={{ color: theme.palette.text.primary, fontSize: 50 }} />
-                <Typography component="h4" sx={titleTypographyStyles}>24/7 Support</Typography>
-                <Typography sx={descTypographyStyles}>
-                  We are providing Round-the-Clock Support Your Success Knows No
-                  Boundaries with Our 24/7 Commitment.
-                </Typography>
-              </Box>
-            </Grid>
-          </Grid>
-        </Box>
-      </Box>
+      <WhyChooseUs />
     </>
   );
 };
--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,15 +1,158 @@
 import React from "react";
 import Banner from "../components/common/Banner";
+import Underline from "../components/ui/Underline";
 import bannerImage from "../assets/bannerAbout.jpeg";
+import { Box, Stack, Typography } from "@mui/material";
+import theme from "../theme";
+
+const ourValues = [
+  {
+    heading: "1. Innovation:",
+    description:
+      "We embrace innovation in everything we do, constantly pushing the boundaries of what's possible.",
+  },
+  {
+    heading: "2. Excellence:",
+    description:
+      "We strive for excellence in quality, service, and execution, setting the highest standards for ourselves and our work.",
+  },
+  {
+    heading: "3. Integrity:",
+    description:
+      "We conduct ourselves with integrity, honesty, and transparency, earning the trust and respect of our clients, partners, and colleagues.",
+  },
+  {
+    heading: "4. Collaboration:",
+    description:
+      "We believe in the power of collaboration, working closely with our clients to understand their needs and deliver tailored solutions.",
+  },
+  {
+    heading: "5. Continuous improvement:",
+    description:
+      "We are committed to continuous improvement, always seeking new ways to enhance our skills, processes, and services.",
+  },
+];
+
+//Styles
+const styles = {
+  sectionPadding: {
+    px: { xs: 3, sm: 6, lg: 14 },
+    py: 12,
+  },
+  heading: {
+    color: theme.palette.text.secondary,
+    fontSize: { xs: "26px", sm: "28px", md: "30px" },
+    fontWeight: 800,
+    fontFamily: theme.typography.fontFamily,
+    mb: 1,
+  },
+  paragraph: {
+    color: theme.palette.custom.paragraph,
+    fontFamily: theme.typography.fontFamilySecondary,
+    fontSize: "16px",
+    fontWeight: 400,
+    lineHeight: "24px",
+  },
+};
 
 const AboutUs = () => {
   return (
-    <Banner
-      title="About Us"
-      subTitle={`Empowering Your Digital\nJourney with Innovative Solutions.`}
-      image={bannerImage}
-      currentPage="about"
-    />
+    <>
+      {/* Banner Section */}
+      <Banner
+        title="About Us"
+        subTitle={`Empowering Your Digital\nJourney with Innovative Solutions.`}
+        image={bannerImage}
+        currentPage="about"
+      />
+
+      {/* About Us Section  */}
+      <Box component="section" sx={styles.sectionPadding}>
+        <Box component="article" aria-labelledby="about-us">
+          <Typography
+            component="h5"
+            sx={{
+              color: theme.palette.text.primary,
+              textTransform: "uppercase",
+              fontSize: "20px",
+              fontFamily: theme.typography.fontFamily,
+              fontWeight: 800,
+              mb: 1,
+            }}
+          >
+            About Us
+          </Typography>
+          <Typography sx={styles.paragraph}>
+            Welcome to Jawam Infotech, where innovation meets expertise to shape
+            the digital future. Established in 2023, we have quickly emerged as
+            a leading software development company dedicated to empowering
+            businesses with cutting-edge technology solutions.
+          </Typography>
+          <Underline
+            lineWidth="140px"
+            lineHeight="4px"
+            barWidth="5px"
+            mtop={1.2}
+            mBottom={{ xs: 5, sm: 5 }}
+          />
+        </Box>
+
+        {/* Main Content Section (Story, Mission, Values) */}
+        <Stack spacing={3}>
+          <Box component="article" aria-labelledby="our-story">
+            <Typography component="h2" sx={styles.heading}>
+              Our Story
+            </Typography>
+            <Typography sx={styles.paragraph}>
+              Founded by a team of passionate technologists, Jawam Infotech was
+              born out of the vision to revolutionise the digital landscape. Our
+              journey began with a simple yet ambitious goal: to provide
+              businesses with transformative software solutions that drive
+              growth and success.
+            </Typography>
+          </Box>
+
+          <Box component="article" aria-labelledby="our-mission">
+            <Typography component="h2" sx={styles.heading}>
+              Our Mission
+            </Typography>
+            <Typography sx={styles.paragraph}>
+              At Jawam Infotech, our mission is to empower businesses to thrive
+              in the digital age. We believe in harnessing the power of
+              technology to solve complex challenges, drive innovation, and
+              create lasting value for our clients.
+            </Typography>
+          </Box>
+
+          <Box component="article" aria-labelledby="our-values">
+            <Typography component="h2" sx={styles.heading}>
+              Our Values
+            </Typography>
+            <Stack spacing={{ xs: 1.7, lg: 1.5 }}>
+              {ourValues.map((item, index) => (
+                <Box key={index}>
+                  <Typography
+                    component="h4"
+                    sx={{
+                      color: theme.palette.text.secondary,
+                      fontSize: { xs: "21px", sm: "23px" },
+                      fontFamily: theme.typography.fontFamily,
+                      fontWeight: 700,
+                      mb: 0.5,
+                    }}
+                  >
+                    {item.heading}
+                  </Typography>
+                  <Typography sx={styles.paragraph}>
+                    {item.description}
+                  </Typography>
+                </Box>
+              ))}
+            </Stack>
+          </Box>
+        </Stack>
+      </Box>
+    </>
   );
 };
 
